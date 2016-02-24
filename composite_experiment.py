2c0c5bde7f7309cb4ba4f4e9e0f578cc22b33f5b
palmagro <palmagroblanco@gmail.com>
2016-02-24 15:17:36 +0100

    Confusion matrices showing percentages

--- composite_experiment.py
+++ composite_experiment.py
@@ -19,7 +19,7 @@ class composite_experiment:
             e = experiment(d[0],7474,user,pswd,d[1],"normal",self.param,self.trainset_p,self.iteraciones)
             x,y,xd,yd = e.ntype_prediction(a,b,jump)
             self.p.line(x, y, color=pal[idx],legend=d[2],line_width=2.0)
-            self.p.line(xd, yd, color=pal[idx],legend=d[2] + "dev",line_width=2.0,line_dash='dotted')
+            #self.p.line(xd, yd, color=pal[idx],legend=d[2] + "dev",line_width=2.0,line_dash='dotted')
             self.p.legend.background_fill_alpha = 0.5
 
     def ltype_prediction(self,a,b,jump):
@@ -28,5 +28,5 @@ class composite_experiment:
             e = experiment(d[0],7474,user,pswd,d[1],"normal",self.param,self.trainset_p,self.iteraciones)
             x,y,xd,yd = e.ltype_prediction(a,b,jump)
             self.p.line(x, y, color=pal[idx],legend=d[2],line_width=2.0)
-            self.p.line(xd, yd, color=pal[idx],legend=d[2] + "dev",line_width=2.0,line_dash='dotted')
+            #self.p.line(xd, yd, color=pal[idx],legend=d[2] + "dev",line_width=2.0,line_dash='dotted')
             self.p.legend.background_fill_alpha = 0.5        
